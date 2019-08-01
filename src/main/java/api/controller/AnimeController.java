6721553734219a9c811bb469a41ff06c35a6a842
Yago <yagogimenez1@hotmail.com>
2019-08-01 14:00:13 -0300

    API do DreamAnimes

--- src/main/java/api/controller/AnimeController.java
+++ src/main/java/api/controller/AnimeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,22 +20,19 @@ public class AnimeController {
 			String url = "https://www.dreamanimes.com.br/anime-info/" + name;
 			Document doc = Jsoup.connect(url).followRedirects(false).timeout(60000).get();
 
-			List<String> fields = Arrays.asList(Arrays.copyOfRange(doc.getElementById("content").getElementsByClass("container").get(0).html().split("\n"), 8, 32));
-			fields = fields.stream().map(s -> s.replaceAll(".*</b>", "").trim()).collect(Collectors.toList());
-			fields.removeIf(s -> s.equalsIgnoreCase("<br>"));
+			List<String> fields = doc.body().getElementById("content").getElementsByClass("container").get(0).textNodes().stream().map(t -> t.getWholeText().trim()).filter(s -> !s.isEmpty()).collect(Collectors.toList());
 
 			final String link = "https://www.dreamanimes.com.br/anime-info/" + name;
 			final String description = doc.body().getElementById("pcontent").getAllElements().get(4).text();
-			final String status = fields.get(3);
-			final String author = fields.get(4).equals("?") ? "Desconhecido" : fields.get(4);
-			final String studio = fields.get(5);
+			final String status = fields.get(2);
+			final String author = fields.get(3).equals("?") ? "Desconhecido" : fields.get(4);
+			final String studio = fields.get(4);
 			final String[] genres = fields.get(0).split(", ");
-			final float score = Float.parseFloat(fields.get(12).replace("</span>", "").replace("<span>", ""));
+			final float score = Float.parseFloat(doc.body().getElementById("content").getElementsByClass("container").get(0).getElementsByTag("span").text());
 			final int episodes = Integer.parseInt(fields.get(1));
-			final int ovas = Integer.parseInt(fields.get(2));
-			final int year = Integer.parseInt(fields.get(7));
+			final int year = Integer.parseInt(fields.get(6));
 
-			return new Anime(link, description, status, author, studio, genres, score, episodes, ovas, year);
+			return new Anime(link, description, status, author, studio, genres, score, episodes, year);
 		} catch (IOException e) {
 			return null;
 		}
--- src/main/java/api/model/Anime.java
+++ src/main/java/api/model/Anime.java
@@ -4,9 +4,9 @@ public class Anime {
 	private String url, description, status, author, studio;
 	private String[] genres;
 	private float score;
-	private int episodes, ovas, year;
+	private int episodes, year;
 
-	public Anime(String url, String description, String status, String author, String studio, String[] genres, float score, int episodes, int ovas, int year) {
+	public Anime(String url, String description, String status, String author, String studio, String[] genres, float score, int episodes, int year) {
 		this.url = url;
 		this.description = description;
 		this.status = status;
@@ -15,7 +15,6 @@ public class Anime {
 		this.genres = genres;
 		this.score = score;
 		this.episodes = episodes;
-		this.ovas = ovas;
 		this.year = year;
 	}
 
@@ -49,10 +48,6 @@ public class Anime {
 
 	public int getEpisodes() {
 		return episodes;
-	}
-
-	public int getOvas() {
-		return ovas;
 	}
 
 	public int getYear() {
