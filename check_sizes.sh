a174fb597267f6bc998bd626962a925a9406153f
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:30:24 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-path="../src/main/resources/reactions";
+path='../src/main/resources/reactions';
 for i in $path; do
-    for img in $i; do
-        echo identify -format "%wx%h" $img;
+    for img in '$path/$i'; do
+        echo identify -format '%wx%h' $img;
     done
 done
