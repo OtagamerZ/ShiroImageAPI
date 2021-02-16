b770e24b7dc0fc96742c1eb7e887ca881387173a
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:31:14 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -2,7 +2,7 @@
 
 path='../src/main/resources/reactions';
 for i in $path; do
-    for img in '$path/$i'; do
+    for img in $path/$i; do
         echo identify -format '%wx%h' $img;
     done
 done
