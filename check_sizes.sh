469c43d51fde309132ea6f0b5e8cb62346a8aad9
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:44:01 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -2,7 +2,10 @@
 
 path='src/main/resources/reactions/*';
 for i in $path; do
-    for img in $i/*; do
-        echo "$(identify -format '%wx%h' $img)";
+    for img in "$i"/*; do
+        size=${$(identify -format '%wx%h' "$img")[x]};
+        if [ ${size[0]} -lt 400 ] || [ ${size[1]} -lt 200 ]; then
+            echo $size;
+        fi
     done
 done
