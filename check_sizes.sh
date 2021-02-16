cc8179e94a97b5bf7aab5488d75c491b4c39a6b9
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:58:14 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -8,7 +8,8 @@ for i in $path; do
         width="$(cut -d'x' -f1 <<< "$size")";
         height="$(cut -d'x' -f2 <<< "$size")";
         if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
-            echo "$size";
+            rm "$img";
         fi
+        mv "$img" "$img"_rev
     done
 done
