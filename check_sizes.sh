0cff7331b89f2397268f32b7eb9fbdd1ac4e43d6
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:49:04 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -4,9 +4,11 @@ path='src/main/resources/reactions/*';
 for i in $path; do
     for img in "$i"/*; do
         size="$(identify -format '%wx%h' "$img")";
-        values="${!size[x]}";
-        if [ "${values[0]}" -lt 400 ] || [ "${values[1]}" -lt 200 ]; then
-            echo "$values";
+
+        width="$(cut -d'x' -f1 <<< "$size")";
+        height="$(cut -d'x' -f2 <<< "$size")";
+        if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
+            echo "$width"'x'"$height";
         fi
     done
 done
