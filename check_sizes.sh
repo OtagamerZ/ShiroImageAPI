23e98fc6584e086cf650d11aa2076183f9ad680f
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 17:35:12 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-path='src/main/resources/reactions';
+path='src/main/resources/reactions/*';
 for i in $path; do
-    echo $i;
-    #for img in $path/$i; do
-    #    echo $img;
-    #done
+    for img in $i/*; do
+        echo $img;
+    done
 done
