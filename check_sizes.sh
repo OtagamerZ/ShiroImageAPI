89cabd8148da6b8303a31549f5ce3edb46bba29d
ygimenez <yagogimenez1@hotmail.com>
2021-02-17 09:31:12 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -5,11 +5,18 @@ shopt -s nullglob
 for i in $path; do
 	shopt -s nullglob
 	for img in "$i"/*.gif; do
-		{
-			filename="$(basename img)"
-			dir="$(dirname img)"
-			size="$(convert "$img" -print "%wx%h" /dev/null)"
+		filename="$(basename img)"
+		dir="$(dirname img)"
+		size="$({
+			convert "$img" -print "%wx%h" /dev/null
+		} || {
+			echo 'fail'
+		})"
 
+		if [ "$size" == 'fail' ]; then
+			echo 'Invalid '"$img"
+			rm "$img"
+		else
 			width="$(cut -d'x' -f1 <<<"$size")"
 			height="$(cut -d'x' -f2 <<<"$size")"
 			if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
@@ -19,10 +26,7 @@ for i in $path; do
 				echo 'Checked '"$img"
 				mv "$img" "$dir"/"$filename".rev
 			fi
-		} || {
-			echo 'Invalid '"$img"
-			rm "$img"
-		}
+		fi
 	done
 
 	shopt -s nullglob
