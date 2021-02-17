6c89fb95afc17ae11369794e09e646a8995b4e8d
ygimenez <yagogimenez1@hotmail.com>
2021-02-17 09:11:52 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -9,19 +9,20 @@ for i in $path; do
 
         width="$(cut -d'x' -f1 <<< "$size")";
         height="$(cut -d'x' -f2 <<< "$size")";
-        echo "$img";
-        #if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
-        #    rm "$img";
-        #else
-        #    mv "$img" "$img"_rev
-        #fi
+        if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
+            echo 'Removing '"$img";
+            rm "$img";
+        else
+            echo 'Checked '"$img";
+            mv "$img" "$img"_rev
+        fi
     done
 
     shopt -s nullglob
-    for img in "$i"/*.gif; do
+    for img in "$i"/*.gif_rev; do
         i=1;
 
-        #mv "$img" "$(printf "$path/%0.3d.gif" $i)";
+        mv "$img" "$(printf "$path/%0.3d.gif" $i)";
         i=$((i + 1))
     done
 done
