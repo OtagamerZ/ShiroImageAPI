207b3ea512b3872e40de0f492f85802ba68a3112
ygimenez <yagogimenez1@hotmail.com>
2021-02-17 09:10:09 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 path='src/main/resources/reactions/*';
+shopt -s nullglob
 for i in $path; do
+    shopt -s nullglob
     for img in "$i"/*.gif; do
         size="$(convert "$img" -print "%wx%h" /dev/null)";
 
         width="$(cut -d'x' -f1 <<< "$size")";
         height="$(cut -d'x' -f2 <<< "$size")";
-        if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
-            rm "$img";
-        else
-            mv "$img" "$img"_rev
-        fi
+        echo "$img";
+        #if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
+        #    rm "$img";
+        #else
+        #    mv "$img" "$img"_rev
+        #fi
     done
 
+    shopt -s nullglob
     for img in "$i"/*.gif; do
         i=1;
 
-        mv "$img" "$(printf "$path/%0.3d.gif" $i)";
+        #mv "$img" "$(printf "$path/%0.3d.gif" $i)";
         i=$((i + 1))
     done
 done
