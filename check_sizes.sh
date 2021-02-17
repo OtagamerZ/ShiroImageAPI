e3f18a3e9b95fd67f41b7e7bdef77b9e3aacfc96
ygimenez <yagogimenez1@hotmail.com>
2021-02-17 09:54:42 -0300

    Update

--- check_sizes.sh
+++ check_sizes.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 path='src/main/resources/reactions/*'
+checked=0
+removed=0
+failed=0
+
 shopt -s nullglob
 for i in $path; do
 	shopt -s nullglob
@@ -12,15 +16,18 @@ for i in $path; do
 		if [[ "$size" == *"fail"* ]]; then
 			echo 'Invalid '"$img"
 			rm "$img"
+			failed=$((failed + 1))
 		else
 			width="$(cut -d'x' -f1 <<<"$size")"
 			height="$(cut -d'x' -f2 <<<"$size")"
 			if [ "$width" -lt 400 ] || [ "$height" -lt 200 ]; then
 				echo 'Removing '"$img"
 				rm "$img"
+				removed=$((removed + 1))
 			else
 				echo 'Checked '"$img"
 				mv "$img" "$dir"/"$filename".rev
+				checked=$((checked + 1))
 			fi
 		fi
 	done
@@ -34,3 +41,21 @@ for i in $path; do
 		i=$((i + 1))
 	done
 done
+
+echo 'Checked: '"$checked"' files'
+echo 'Removed: '"$removed"' files'
+echo 'Failed: '"$failed"' files'
+
+echo 'Do you want to commit those files? (s/n)'
+read -r commit
+
+while [[ "$commit" != "s" ]] && [[ "$commit" != "n" ]]; do
+	echo 'Please write either ''s'' or ''n'''
+	read -r commit
+done
+
+if [[ "$commit" == "s" ]]; then
+	git add .
+	git commit -m "Update"
+	git push
+fi
